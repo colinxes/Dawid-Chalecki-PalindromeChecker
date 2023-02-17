cae64eebd43c6169811f6968acaec33397b41c24
Colin Eikis-Sagcob <74212344+colinxes@users.noreply.github.com>
2023-02-17 16:25:39 +0100

    Init repo

---
+++ PalindromeChcecker/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PalindromeChcecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user!");
+
+            Console.WriteLine("Here you can check if words are palindromes");
+
+            string input = "";
+            // Read user input
+            input = Console.ReadLine();
+            char[] inputAsArray = input.ToCharArray();
+            string output = "";
+            int startPoint = inputAsArray.Length - 1;
+
+            for (int i = startPoint; // DEFINE START POINT
+                i >= 0; // CONDITION HOW LONG THE LOOP SHOULD EXECUTE
+                i-- ) // WHAT SHOULD BE DONE AFTER EACH LOOP             
+            {
+                output += inputAsArray[i];
+            }
+
+            if (input.ToLower() == output.ToLower())
+            {
+                Console.WriteLine("It is an palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("It is not an palindrome!");
+            }
+        }
+    }
+}
